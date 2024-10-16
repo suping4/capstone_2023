318c224db8a42b0b2090008ba7a13f2c699012ab
Junsup Kim <88069526+suping4@users.noreply.github.com>
2024-10-16 15:06:59 +0900

    Update hangul_processor.h

--- braille_oop/hangul_processor.h
+++ braille_oop/hangul_processor.h
@@ -2,14 +2,14 @@
 #define HANGUL_PROCESSOR_H
 
 #include <Arduino.h>
+#include "braille.h"  // braille 헤더 파일 포함
+
 
 class HangulProcessor {
 public:
     static void split_han_cho_jung_jong(char byte1, char byte2, char byte3, 
                                         unsigned int &cho, unsigned int &jung, unsigned int &jong);
     static void han_braille(int index1, int index2, int index3, Braille &bra);
-
-private:
     static const byte hangul_cho[19];
     static const byte hangul_jung[21];
     static const byte hangul_jong[28];
